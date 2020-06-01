204c9a298bb9ab4d64cd9ef96209fef1b35bb5ac
kyeongho664 <rudgh664@naver.com>
2020-06-01 17:56:48 +0900

    kyeongho
    boardController 뷰단과 연결

--- src/main/java/com/khfinal/project/board/controller/boardController.java
+++ src/main/java/com/khfinal/project/board/controller/boardController.java
@@ -1,5 +1,17 @@
 package com.khfinal.project.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class boardController {
 
+	@RequestMapping("/board/board.do")
+	public ModelAndView noticeList() {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("board/board");
+		return mav;
+	}
 }
