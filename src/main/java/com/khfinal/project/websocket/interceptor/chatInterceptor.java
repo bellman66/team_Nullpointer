a29f920819bf882f93a8157e8ebbbd4e552985a2
Yun <rndso15@naver.com>
2020-06-15 15:29:53 +0900

    update version 0.4
    오류 해결버전

--- src/main/java/com/khfinal/project/websocket/interceptor/chatInterceptor.java
+++ src/main/java/com/khfinal/project/websocket/interceptor/chatInterceptor.java
@@ -9,11 +9,35 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 
 public class chatInterceptor extends HttpSessionHandshakeInterceptor{
 
+	
+	@Override
+	public void setCreateSession(boolean createSession) {
+		// TODO Auto-generated method stub
+		super.setCreateSession(createSession);
+	}
+
+	
 	@Override
 	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
 			Map<String, Object> attributes) throws Exception {
-		System.out.println("hand shake start");
+		
+		String[] originuri = request.getURI().getQuery().split("&");
+		String[] splitvalue = originuri[0].split("=");
+		String id = splitvalue[1];
+		
+		splitvalue = originuri[1].split("=");
+		String user_name;
+		if(splitvalue.length < 2) {
+			user_name = "user";
+		}
+		else user_name = splitvalue[1];
+		
+		attributes.put("id", id);
+		attributes.put("user_name", user_name);
+		
 		return super.beforeHandshake(request, response, wsHandler, attributes);
 	}
+	
+	
 	
 }
